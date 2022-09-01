e30f768c196a2c25b2648694047d513df4486f7d
RafaelTC3 <rafael.crivelente-ciandt@faci.ly>
2022-09-01 09:22:08 -0300

    Changing router run address

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	router.GET("/albums/:id", getAlbumsByID)
 	router.POST("/albums", postAlbums)
 
-	router.Run("localhost:8080")
+	router.Run("0.0.0.0:8080")
 }
 
 func getAlbums(c *gin.Context) {
